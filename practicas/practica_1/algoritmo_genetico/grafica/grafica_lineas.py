97dfcdacd0a068092a5eca13b6e5ccc46d942f8e
Octavio Arroyo <oarroyo@UbuntuLXC01L02.L02>
2026-03-23 13:45:14 -0600

    Se edito la forma de guardar las graficas

--- practicas/practica_1/algoritmo_genetico/grafica/grafica_lineas.py
+++ practicas/practica_1/algoritmo_genetico/grafica/grafica_lineas.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 class Grafica_lineas(Grafica):
 
-    def generar_grafica(self, datos: list[float], etiqueta: str = 'Sin etiqueta') -> None:
+    def generar_grafica(self, datos: list[float], etiqueta: str = 'Sin etiqueta', guardar_grafica ='No guardar') -> None:
     
         tamano = len(datos)
         x = list(range(tamano)) 
@@ -24,4 +24,12 @@ class Grafica_lineas(Grafica):
 
         plt.legend()
         plt.grid(True)
-        plt.show()
+
+        if not guardar_grafica == 'No guardar':
+            guardar: str = "/home/oarroyo/computo_evolutivo_proyectos/datos_experimentos/imagenes" + guardar_grafica
+            plt.savefig(guardar, dpi=300)
+            plt.close()  
+            return
+        else:
+            plt.show()
+            return
